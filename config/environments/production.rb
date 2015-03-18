4ed73a5c6655e3342aeea69e2938814416aba0e5
Sahil Dhankhar <sahil1345@gmail.com>
2015-03-18 20:12:19 +0530

    config file fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
    :address => 'smtp.gmail.com',
     :port => 587,
     :domain => "local.drop.me:300",
-    :user_name => ENV[GMAIL_USER_NAME],
-    :password => ENV[GMAIL_PASSWORD],
+    :user_name => ENV["GMAIL_USER_NAME"],
+    :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true
   }
